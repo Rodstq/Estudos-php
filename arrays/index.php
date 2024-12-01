0e6539d89e9461f7b836bf74e33410b091752116
Rodstq <rodolfovanhouten@gmail.com>
2024-12-01 16:56:12 -0300

    finishing basic array study

--- arrays/index.php
+++ arrays/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    #criar array
+    #criar array numérico
     $valores = [1,2,3,4,5,6,7,8,9];
 
     #mudar o valor do indice 1 para o número 3
@@ -9,13 +9,33 @@
     #adicionar 20 no final do array
     $valores[] = 20;
 
+    #array assossiativo
+    $dados = [
+        'A' => 10,
+        'B' => 20,
+        'C' => 30
+    ];
 
+    #arrays multidimensionais
+    $matriz = [
+        [1,2,3],
+        [2,1,3],
+        [2,3,1]
+    ];
+
+    #multidimensionais assossiativos
+    $cidades = [
+        'Brasil' => ['São Paulo', 'Rio de Janeiro'],
+        'Portugal' => ['Lisboa', 'Porto'],
+        'Espanha' => ['Madrid', 'Barcelona',]
+    ];
+
+    #definição de uma função tipo toString()
     function to_mostrar($array){
         for ($i = 0 ; $i < count($array); $i++){
             echo "<p class = \"texto-retorno\">" . $array[$i] . "</p>";
         }
     }
-    
 ?>
 
 
